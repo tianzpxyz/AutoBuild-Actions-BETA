88a871f5ce2e7c6137886fba34f3522776a3486a
tianzpxyz <63276801+tianzpxyz@users.noreply.github.com>
2024-01-28 10:18:13 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -24,7 +24,7 @@ Firmware_Diy_Core() {
 	Default_IP="192.168.1.1"
 	# 固件 IP 地址
 	
-	Default_Title="Powered by AutoBuild-Actions"
+	Default_Title="Powered by ATPXX1"
 	# 固件终端首页显示的额外信息
 	
 	Short_Fw_Date=true
